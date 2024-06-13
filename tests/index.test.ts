7e9a8b289a852cc4a3f0133d9058074ee8550d06
dffrs <dffrs@iscte-iul.pt>
2024-06-13 13:13:45 +0100

    test: check that after changing an obsverved attr the other attrs don't change at all

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -30,7 +30,7 @@ describe("Spy tests", () => {
     // change attr1 to this new value
     const newValueForAttr1 = 99;
 
-    // create a varible to store value that comes from spyMethod
+    // create a variable to store value that comes from spyMethod
     let oldValueRecordedInsideSpyMethod: number = -1; // initial value - does not matter
     let newValueRecordedInsideSpyMethod: number = -1; // initial value - does not matter
 
@@ -41,7 +41,7 @@ describe("Spy tests", () => {
       newValueRecordedInsideSpyMethod = newValue;
     };
 
-    // obsverve first attr
+    // observe first attr
     spy.observe("attr1", spyMethod);
 
     // change attr1
@@ -51,4 +51,43 @@ describe("Spy tests", () => {
     expect(oldValueRecordedInsideSpyMethod).toBe(initialAttr1);
     expect(newValueRecordedInsideSpyMethod).toBe(newValueForAttr1);
   });
+
+
+  it("observe how changing first attr does NOT change the others", () => {
+    // create initial values
+    const initialAttr1 = 10;
+    const initialAttr2 = "Name";
+
+    // Create an instance of spy  dummyClass
+    const spy = new SpyOnDummy(initialAttr1, initialAttr2);
+
+    // create variable to store number of calls
+    let numberOfCalls = 0;
+    // change attr2 to this new value
+    const newValueForAttr2 = "This was changed";
+
+    // create a variable to store value that comes from spyMethod
+    let oldValueRecordedInsideSpyMethod: string = ""; // initial value - does not matter
+    let newValueRecordedInsideSpyMethod: string = "";// initial value - does not matter
+
+    // create a spy method to increment numberOfCalls
+    const spyMethod = (newValue: string, oldValue: string) => {
+      numberOfCalls++;
+      oldValueRecordedInsideSpyMethod = oldValue;
+      newValueRecordedInsideSpyMethod = newValue;
+    };
+
+    // observe first attr2
+    spy.observe("attr2", spyMethod);
+
+    // change attr2
+    spy.attr2 = "This was changed";
+
+    expect(numberOfCalls).toBe(1);
+    expect(spy.attr2).toBe(newValueForAttr2);
+    expect(oldValueRecordedInsideSpyMethod).toBe(initialAttr2);
+    expect(newValueRecordedInsideSpyMethod).toBe(newValueForAttr2);
+
+    expect(spy.attr1).toBe(10) // This should still have it's original value 
+  })
 });
